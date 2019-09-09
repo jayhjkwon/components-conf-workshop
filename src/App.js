f3d9d7e805bcb44b56aedb55e03cb1aa5068a4f0
Jay Kwon <jay.kwon@rea-group.com>
2019-09-09 11:47:07 +1000

    authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+import { Auth } from 'aws-amplify'
 
 function App() {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log({ user }))
+      .catch(error => console.log({ error }))
+  })
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true })
